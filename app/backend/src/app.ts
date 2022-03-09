e2fba14f4d9f0990576cf09260ed0ed931b78347
Rodrigo G. Oliveira <mjgargani@gmail.com>
2022-03-09 13:32:44 -0300

    patch from https://github.com/betrybe/sd-0x-trybe-futebol-clube-rubric/commit/a93afb9f159a8f81e99f3b370db2aa766c5d448e

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,26 @@
+import * as express from 'express';
+
 class App {
-  public app: any;
+  public app: express.Express;
+  // ...
+
+  constructor() {
+    // ...
+    this.config();
+    // ...
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+    // ...
+  }
 
   // ...
   public start(PORT: string | number):void {
