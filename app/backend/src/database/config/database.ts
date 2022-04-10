c58f2c94bdf9ed52997842eb8e418ea19ca96495
Rodrigo G. Oliveira <mjgargani@gmail.com>
2022-04-10 09:57:38 -0300

    adapt base for 26.2 ts-sequelize example

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,14 +1,17 @@
-require('dotenv').config();
+import 'dotenv/config';
+import { Options } from 'sequelize';
 
-module.exports = {
+const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || '1234',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || '3002',
+  port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
   },
   logging: false,
-};
+}
+
+module.exports = config;
