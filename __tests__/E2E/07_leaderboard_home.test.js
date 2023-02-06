4c2421649371ed256f65997a8b969a8a07599e5a
Iago <iago.pferreiravr@gmail.com>
2023-02-06 18:13:21 -0300

    implementando testes dos novos requisitos 23 e 24

--- __tests__/E2E/07_leaderboard_home.test.js
+++ __tests__/E2E/07_leaderboard_home.test.js
@@ -8,6 +8,7 @@ const { insertFinished } = require('../utils/inserts');
 const { teams } = require('../expected_results/trybe_football_club');
 const { puppeteerDefs, containerPorts } = require('../config/constants');
 const { getRequirement } = require('../utils/util');
+const axios = require('axios').default;
 
 let database, browser, page;
 
@@ -31,8 +32,68 @@ const endpoint = '/leaderboard/home'
 const twoGoals = '2';
 const oneGoal = '1';
 
-describe(getRequirement(29), () => {
-  it('Será avaliado que ao fazer a requisição ao endpoint /leaderboard/home será retonado os campos e valores corretos considerando os dados iniciais do banco de dados', async () => {
+const generateRandomNumber = () => Math.floor(Math.random() * homeResult1.length);
+
+const getTeamsToCompare = () => {
+  let counter = 0;
+  const teamsToCheck = {};
+  const qtdTeamsToCheck = Math.ceil(Math.random() * 4);
+
+  while (counter < qtdTeamsToCheck) {
+    const index = generateRandomNumber();
+    const possibleTeamToCheck = homeResult1[index]
+
+    if (!(index in teamsToCheck)) {
+      teamsToCheck[index] = possibleTeamToCheck;
+      counter += 1;
+    }
+  }
+  return teamsToCheck;
+}
+
+describe(getRequirement(23), () => {
+  let result;
+  const teamsToCompare = Object.entries(getTeamsToCompare());
+  beforeAll(async () => {
+    result = await axios
+    .get(
+      `${URL(containerPorts.backend).BASE_URL}${endpoint}`,
+      )
+      .then((response) => response)
+      .catch(({ response: { status, data: { message } } }) => ({ status, message }));
+
+      expect(result).toHaveProperty("status");
+      expect(result.status).toBe(200);
+  })
+
+  it.each(teamsToCompare)('Serão validados os dados do time na %dª posição', async (index, teamToCompare) => {
+      const team = result.data[index];
+
+      expect(team).toHaveProperty('name');
+      expect(team.name).toBe(teamToCompare.name);
+
+      expect(team).toHaveProperty('totalPoints');
+      expect(team.totalPoints).toBe(Number.parseInt(teamToCompare.totalPoints));
+
+      expect(team).toHaveProperty('totalGames');
+      expect(team.totalGames).toBe(Number.parseInt(teamToCompare.totalGames));
+
+      expect(team).toHaveProperty('totalVictories');
+      expect(team.totalVictories).toBe(Number.parseInt(teamToCompare.totalVictories));
+
+      expect(team).toHaveProperty('totalLosses');
+      expect(team.totalLosses).toBe(Number.parseInt(teamToCompare.totalLosses));
+
+      expect(team).toHaveProperty('goalsFavor');
+      expect(team.goalsFavor).toBe(Number.parseInt(teamToCompare.goalsFavor));
+
+      expect(team).toHaveProperty('goalsOwn');
+      expect(team.goalsOwn).toBe(Number.parseInt(teamToCompare.goalsOwn));
+  });
+});
+
+describe(getRequirement(24), () => {
+  it('Será avaliado que ao fazer a requisição ao endpoint /leaderboard/home será retornado os campos e valores corretos considerando os dados iniciais do banco de dados', async () => {
     await page.waitForTimeout(puppeteerDefs.pause.brief);
     await page.select(leaderboard.table.filter.select, 'Classificação Mandantes')
     const classificationButton = await page.$(leaderboard.table.filter.button)
